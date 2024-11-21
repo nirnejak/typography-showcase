cf77255328a3662c8cd54ebaaf5f78f30fc23743
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-11-21 18:59:23 +0530

    App config added

---
+++ config.ts
@@ -0,0 +1,23 @@
+const config = {
+  baseUrl: "https://example.com",
+  appName: "Typography Showcase",
+  appDescription: "A gallery of beautiful fonts",
+  creator: "Jitendra Nirnejak",
+  authorName: "Jitendra Nirnejak",
+  authorUrl: "https://nirnejak.com/",
+  keywords: [
+    "Remix",
+    "React",
+    "Vite",
+    "TypeScript",
+    "JavaScript",
+    "TailwindCSS",
+    "Framer Motion",
+    "Akar Icons",
+    "ESLint",
+    "Prettier",
+  ],
+  twitterSite: "@site",
+  twitterCreator: "@creator",
+};
+export default config;
